c8873a3b7f8c4748dc5b96a86eba248d371a76d1
William Long <will@williamlong.xyz>
2021-08-06 04:48:35 +0000

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/*H**********************************************************************
+* FILENAME :        main.c             DESIGN REF: FMCM00
+*
+* DESCRIPTION :
+*       File compression and decompression routines.
+*       DESCRIBE THE PROBLEM THAT THIS PROGRAM WAS WRITTEN TO SOLVE.
+*
+* LANGUAGE:  C (tested using gcc)
+* TO COMPILE:  gcc main.c
+*
+*
+* NOTES :
+*       This program and it's source code are licensed under the GNU GPL v2 which gives you the following freedoms:
+*               The freedom to run the program as you wish, for any purpose (freedom 0).
+*               The freedom to study how the program works, and change it so it does your computing as you wish (freedom 1). Access to the source code is a precondition for this.
+*               The freedom to redistribute copies so you can help others (freedom 2).
+*               The freedom to distribute copies of your modified versions to others (freedom 3). By doing this you can give the whole community a chance to benefit from your changes. Access to the source code is a precondition for this.
+*
+* AUTHOR :    Xarvveron        START DATE :    5 Aug 2021
+*
+*
+*H*/
+
+int main() {
+    return 0;
+}
